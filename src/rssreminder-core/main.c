8e7fab9238dc2f44a0e1522f71fb3bd78933d7ef
glace <glacebai@gmail.com>
2011-10-09 14:03:58 +0800

    add simple fetchWord() function to rssreminder-core/main.c

--- src/rssreminder-core/main.c
+++ src/rssreminder-core/main.c
@@ -29,3 +29,72 @@
   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+#include <stdio.h>
+
+int isodd(int num){
+    return num % 2;
+}
+
+int specialSymbol(char ch){
+    return ch == '=' || ch == '<' || ch == '>';
+}
+
+int errReport(const char * errinfo){
+    printf("%s\n",errinfo);
+    return 0;
+}
+
+int fetchWord(char * wordlist, char * word, int * offset){
+    int total=0;
+    char tword[100];
+    int double_quote = 0;
+    int single_quote = 0;
+    int backslash = 0;
+    int len, i;
+    
+    while (*offset<strlen(wordlist) && !isgraph(wordlist[*offset])) (*offset)++;
+    if (*offset>=strlen(wordlist)) return 0;
+    while (*offset<strlen(wordlist) 
+        && ( (isgraph(wordlist[*offset]) && !specialSymbol(wordlist[*offset])) 
+            || isodd(single_quote) || isodd(double_quote))){
+                
+        tword[total] = tolower(wordlist[*offset]);
+        
+        if (tword[total] == '\''){
+            if (single_quote > 0 || double_quote == 0){
+                if (single_quote > 0 && backslash) single_quote--;
+                single_quote++;
+            }
+        }
+        if (tword[total] == '"'){
+            if (double_quote > 0 || single_quote == 0){
+                if (double_quote > 0 && backslash) double_quote--;
+                double_quote++;
+            }
+        }
+        if (tword[total] == '\\') backslash = !backslash;
+        else backslash = 0;
+            
+        total++;
+        (*offset)++;
+    }
+    if (total == 0)
+        while (*offset<strlen(wordlist) && specialSymbol(wordlist[*offset])){
+            tword[total] = tolower(wordlist[*offset]);
+            total++;
+            (*offset)++;
+        }
+    tword[total]='\0';
+    
+    len = strlen(tword);
+    if (len >= 2){
+        if (tword[0] == tword[len-1] && (tword[0] == '\'' || tword[0] == '"')){
+            for (i=0;i<len-2;i++) tword[i] = tword[i+1];
+            tword[len-2] = '\0';
+        }
+    }
+        
+    strcpy(word, tword);
+    return 1;
+}
