b5e709e0f17217fd88af119817bffff65016f692
glace <glacebai@gmail.com>
2011-10-16 20:49:52 +0800

    for testing

---
+++ test/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./rssreminder-core a > out1
+./rssreminder-core a.html > out2
+./rssreminder-core d > out3
+./rssreminder-core d222 > out4
+./rssreminder-core Google+.html > out5  #this command needs about 1 miniute to run =.=|||
+
